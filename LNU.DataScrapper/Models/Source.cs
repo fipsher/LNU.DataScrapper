8ffbdfe737ab0e112bbf757f0d1db6d7ea41b662
Andrii Zvarych Tarasovuch <fipsher@gmail.com>
2018-03-25 16:26:19 +0300

    Add project files.

---
+++ LNU.DataScrapper/Models/Source.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNU.DataScrapper
+{
+    public class Source
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ LNU.DataScrapper/Program.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using System.Configuration;
+using System.Linq;
+
+namespace LNU.DataScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = ConfigurationManager.AppSettings["Url"];
+            var connectionString = ConfigurationManager.AppSettings["RssConnectionString"];
+
+            CNNDataProvider provider = new CNNDataProvider(url);
+
+            var list = provider.GetRSS().Result;
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("rss");
+            var collection = database.GetCollection<Article>("article");
+
+            foreach (var item in list)
+            {
+                var elementExist = collection.AsQueryable().Any(el => el.Title == item.Title && item.PublishedAt == el.PublishedAt);
+                if (!elementExist)
+                {
+                    collection.InsertOne(item);
+                }
+            }
+        }
+    }
+}
